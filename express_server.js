a1129ad6b92efabd6990b9c6cd1cc3a5aa9acc05
Maya Pfeiffer <mayap93@hotmail.com>
2023-07-11 14:54:36 -0400

    Include delete buttons on both client and routing for it on server side

--- express_server.js
+++ express_server.js
@@ -25,17 +25,19 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
-})
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
+});
+
+app.post("/urls/:id/delete", (req, res) => {
+  const id = req.params.id;
+  delete urlDatabase[id];
+  res.redirect('/urls');
 });
 
 app.get("/urls/:id", (req, res) => {
