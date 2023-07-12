88342735446717f12a7d29584aa9067095dc4949
Maya Pfeiffer <mayap93@hotmail.com>
2023-07-12 12:50:10 -0400

    Update to include logout route in server

--- express_server.js
+++ express_server.js
@@ -88,3 +88,8 @@ app.post("/login", (req, res) => {
   res.cookie("username", username);
   res.redirect('/urls');
 })
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
+});
