6b94a995b99f9e25c01039a7b89a346db9374bf8
Maya Pfeiffer <mayap93@hotmail.com>
2023-07-12 21:24:40 -0400

    Re-impementation of registration, login, logout

--- express_server.js
+++ express_server.js
@@ -6,7 +6,7 @@ const PORT = 8080;
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser())
+app.use(cookieParser());
 
 function generateRandomString(length = 6) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -16,15 +16,38 @@ function generateRandomString(length = 6) {
     shortURLId += characters[randomIndex];
   }
   return shortURLId;
-}
+};
+
+function findUserByEmail(email) {
+  for (const userID in users) {
+    if (users[userID].email === email) {
+      return users[userID];
+    }
+  }
+  return null;
+};
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
 app.use((req, res, next) => {
-  res.locals.username = req.cookies["username"];
+  const userId = req.cookies["user_id"];
+  res.locals.user = users[userId];
   next();
 });
 
@@ -42,20 +65,23 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/urls", (req, res) => {
   const templateVars = {
-    username: req.cookies["username"],
+    user_id: res.locals.user,
     urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    user: res.locals.user
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls/:id/delete", (req, res) => {
   const id = req.params.id;
   delete urlDatabase[id];
-  res.redirect("/urls");
+  res.redirect('/urls');
 });
 
 app.post("/urls/:id", (req, res) => {
@@ -66,7 +92,11 @@ app.post("/urls/:id", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    user: res.locals.user,
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id] 
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -84,16 +114,47 @@ app.get("/u/:id", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
-  const username = req.body.username;
-  res.cookie("username", username);
+  const { email, password} = req.body;
+  const user = Object.values(users).find(user => user.email === email);
+  if (!user || user.password !== password) {
+    res.status(403).send("Error 403: Invalid email or password");
+    return;
+  }
+  res.cookie("user_id", user.id);
   res.redirect("/urls");
-})
+});
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect("/urls");
+  res.clearCookie("user_id");
+  res.redirect("/login");
 });
 
 app.get("/register", (req, res) => {
 res.render("urls_register")
+});
+
+app.post("/register", (req, res) => {
+  const userID = generateRandomString();
+  const { email, password } = req.body
+  if (!email || !password) {
+    res.status(400).send("Error 400: Email and password are required.");
+    return;
+  }
+  const existingUser = Object.values(users).find(user => user.email === email);
+  if (existingUser) {
+    res.status(400).send("Error 400: User already exsists.");
+    return;
+  }
+  const newUser = {
+    id: userID,
+    email: email,
+    password: password
+  };
+  users[userID] = newUser;
+  res.cookie("user_id", userID);
+  res.redirect("/urls");
+})
+
+app.get("/login", (req, res) => {
+  res.render("urls_login");
 })
